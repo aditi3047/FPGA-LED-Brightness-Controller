0f55049fd48f825ce48f70191295da4071717197
aditi <109781226+aditi3047@users.noreply.github.com>
2026-07-14 13:23:04 -0700

    Update PWM.vhd

--- src/PWM.vhd
+++ src/PWM.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06/29/2026 09:49:34 PM
--- Design Name: 
--- Module Name: PWM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
